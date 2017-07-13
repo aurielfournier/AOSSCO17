428f62f393c921436d237c2e8eabf54b219a2a07
aurielfournier <aurielfournier@gmail.com>
2017-07-13 16:44:39 -0500

    working on ggplot

--- 2_ggplot2.R
+++ 2_ggplot2.R
@@ -11,49 +11,46 @@ library(gridExtra)
 library(gapminder) 
 
 # ggplot2 is built on the grammar of graphics, the idea that any plot can be expressed from the same set of components: 
-# a **data** set
-# a **coordinate system**
-# and a set of **geoms** --the visual representation of data points.
 
-# The key to understanding ggplot2 is thinking about a figure in layers.
-# This idea may be familiar to you if you have used image editing programs like Photoshop#, Illustrator, or Inkscape.
+#------ a **data** set
+#------ a **coordinate system**
+#------ and a set of **geoms** --the visual representation of data points.
 
+# The key to understanding ggplot2 is thinking about a figure in layers. This idea may be familiar to you if you have used image editing programs like Photoshop, Illustrator, or Inkscape.
 # Let's start off with an example:
+
+ggplot(data = gapminder, 
+       aes(x = gdpPercap, y = lifeExp))
+
+# this gives us a plot, with no data on it, but for a good reason. 
+# we have told ggplot what data we want to use, and what axis we want
+# the variables to be on, but we haven't told it how we want the data displayed
+# do we want points? lines? box plots? it has no idea
+# so it gives us the blank canvas
+# so lets add a layer
 
 ggplot(data = gapminder, 
        aes(x = gdpPercap, y = lifeExp)) +
   geom_point()
 
-# So the first thing we do is call the `ggplot` function. This function lets R
-# know that we're creating a new plot, and any of the arguments we give the
-# `ggplot` function are the *global* options for the plot: they apply to all
-# layers on the plot.
+# now we can see the data! huzzah!
+# we use a '+' sign to connect layers in ggplot, instead of the %>% pipe that we 
+# learned earlier. I realize this can be confusing
 
+
+
+# lets go into a bit more detail of what the above code says
 # We've passed in two arguments to `ggplot`. First, we tell `ggplot` what data we
 # want to show on our figure, in this example the gapminder data we read in
-# earlier. For the second argument we passed in the `aes` function, which
+# earlier. 
+# For the second argument we passed in the `aes` function, which
 # tells `ggplot` how variables in the **data** map to *aesthetic* properties of
-# the figure, in this case the **x** and **y** locations. Here we told `ggplot` we
-# want to plot the "gdpPercap" column of the gapminder data frame on the x-axis, and
-# the "lifeExp" column on the y-axis. Notice that we didn't need to explicitly
-# pass `aes` these columns (e.g. `x = gapminder[, "gdpPercap"]`), this is because
-# `ggplot` is smart enough to know to look in the **data** for that column!
-  
-#  By itself, the call to `ggplot` isn't enough to draw a figure:
+# the figure, in this case the **x** and **y** variables. 
+# Here we told `ggplot` we want to plot the "gdpPercap" column of the gapminder 
+# data frame on the x-axis, and the "lifeExp" column on the y-axis. 
+# Notice that we didn't need to explicitly pass `aes` these columns (e.g. `x = gapminder[, "gdpPercap"]`), this is because `ggplot` is smart enough to know to look in the **data** for that column!
 
-ggplot(data = gapminder, 
-       aes(x = gdpPercap, y = lifeExp))
-
-#We need to tell `ggplot` how we want to visually represent the data, which we
-#do by adding a new **geom** layer. In our example, we used `geom_point`, which
-#tells `ggplot` we want to visually represent the relationship between **x** and
-#**y** as a scatterplot of points:
-
-ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
-  geom_point()
-
-# Modify the example so that the figure visualise how life expectancy has
-# changed over time:
+# What do we need to change to look at how life expectancy changes over time?  
 
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp)) + 
@@ -64,31 +61,39 @@ ggplot(data = gapminder,
 
 ### Challenge 2 
 
-
 # In the previous examples and challenge we've used the `aes` function to tell
 # the scatterplot **geom** about the **x** and **y** locations of each point.
 # Another *aesthetic* property we can modify is the point *color*. Modify the
 # code from the previous challenge to **color** the points by the "continent"
 # column. What trends do you see in the data? Are they what you expected?
 
+############################
+# ---- Layers
+############################
 
 
-## Layers
-  
 #Using a scatterplot probably isn't the best for visualising change over time.
 #Instead, let's tell `ggplot` to visualise the data as a line plot:
   
 ggplot(data = gapminder, 
        aes(x=year, y=lifeExp, 
-            by=country,color=continent)) +
+            color=continent)) +
   geom_line()
 
-#Instead of adding a `geom_point` layer, we've added a `geom_line` layer. We've
-#added the **by** *aesthetic*, which tells `ggplot` to draw a line for each
-#country.
+#Instead of adding a `geom_point` layer, we've added a `geom_line` layer. 
+# but this probably doesn't look the way we expect
+# since we have the lines colored by continent, there is one line per continent,
+# so all the countries in that continent are merged together
+# if we add the **by** *aesthetic*, we will be able to say do each line
+# by country, and see a different chart
 
-#But what if we want to visualise both lines and points on the plot? We can
-#simply add another layer to the plot:
+ggplot(data = gapminder, 
+       aes(x=year, y=lifeExp, 
+           by=country,color=continent)) +
+  geom_line()
+
+# But what if we want to visualise both lines and points on the plot? 
+# We can simply add another layer to the plot:
   
 ggplot(data = gapminder, 
        aes(x=year, y=lifeExp, 
@@ -106,18 +111,17 @@ ggplot(data = gapminder,
   geom_line(aes(color=continent)) + 
   geom_point()
 
-#In this example, the *aesthetic* mapping of **color** has been moved from the
-#global plot options in `ggplot` to the `geom_line` layer so it no longer applies
-#to the points. Now we can clearly see that the points are drawn on top of the
-#lines.
+# In this example, the *aesthetic* mapping of **color** has been moved from the
+# global plot options in `ggplot` to the `geom_line` layer so it no longer applies
+# to the points. Now we can clearly see that the points are drawn on top of the
+# lines.
 
-### Challenge 3 {.challenge}
 # Switch the order of the point and line layers from the previous example. What happened?
 
   ## Transformations and statistics
   
-#  Ggplot also makes it easy to overlay statistical models over the data. To
-#demonstrate we'll go back to our first example:
+#  ggplot also makes it easy to overlay statistical models over the data. To
+# demonstrate we'll go back to our first example:
 
 ggplot(data = gapminder, 
        aes(x = gdpPercap, 
