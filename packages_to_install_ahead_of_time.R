33e87f733c0f247901d5e16ce0238974f5cc1bcb
aurielfournier <aurielfournier@gmail.com>
2017-07-19 16:34:14 -0500

    script of packages to install ahead of time

---
+++ packages_to_install_ahead_of_time.R
@@ -0,0 +1,18 @@
+# Run this Script
+# It will install the packages that you need
+
+# If you have any difficulties let Auriel and Matt know!
+
+install.packages(c("gapminder","tidyr","dplyr",
+                   "ggplot2",'ggthemes',"RColorBrewer",
+                   "gridExtra"))
+
+# to make sure everything installed correctly run these lines
+
+library(gapminder)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(RColorBrewer)
+library(gridExtra)
